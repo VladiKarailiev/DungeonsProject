0af0cf65f90621ca9ed7fd873292720edd3ae6ee
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-01-26 15:57:10 +0200

    Actor Interface added

--- src/bg/sofia/uni/fmi/mjt/dungeons/entity/actor/Actor.java
+++ src/bg/sofia/uni/fmi/mjt/dungeons/entity/actor/Actor.java
@@ -1,5 +1,50 @@
 package bg.sofia.uni.fmi.mjt.dungeons.entity.actor;
 
+import bg.sofia.uni.fmi.mjt.dungeons.entity.treasure.equippable.Spell;
+import bg.sofia.uni.fmi.mjt.dungeons.entity.treasure.equippable.Weapon;
+
 public interface Actor {
 
+    /**
+     * Tries to assign a weapon to Actor
+     *
+     * @param weapon the weapon to be equipped
+     * @return nothing
+     * @throws IllegalArgumentException if the weapon is null
+     */
+    void equip(Weapon weapon);
+
+    /**
+     * Tries to assign a spell to Actor
+     *
+     * @param spell the spell to be learned
+     * @return nothing
+     * @throws IllegalArgumentException if the spell is null
+     */
+    void learn(Spell spell);
+
+    /**
+     * Adds healing points to Actor
+     *
+     * @param amount the amount to be added
+     * @return nothing
+     * @throws IllegalArgumentException if the amount is not in [0,1000]
+     */
+    void takeHealing(Integer amount);
+
+    /**
+     * Applies damage to Actor
+     *
+     * @param amount the amount to be applied
+     * @return nothing
+     * @throws IllegalArgumentException if the amount is not in [0,1000]
+     */
+    void takeDamage(Integer amount);
+
+    /**
+     * Checks the vital signs of the Actor ;)
+     *
+     * @return if the Actor is alive
+     */
+    boolean isAlive();
 }
